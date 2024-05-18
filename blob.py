718d78aa2cd6a09db5ad50b6325fb7b51c331499
Kashew Knutt <kashewknutt@gmail.com>
2024-05-18 23:28:35 +0530

    blob

--- blob.py
+++ blob.py
@@ -6,12 +6,13 @@ class blob:
     def __init__(self, gender, age, environmentInstance):  #gender : true = male
         self.gender=gender
         self.age=age
-        self.environement = environmentInstance
+        self.environment = environmentInstance
         self.energy = 50
         self.x = random.randint(0, self.environment.dimension - 1)
         self.y = random.randint(0, self.environment.dimension - 1)
         self.environment.space[self.x,self.y] = [0, 0, 150]
         self.energy_for_movement = 10
+        self.food_content = 30
 
     def movement(self):
         self.environment.space[self.x, self.y] = [0, 0, 0]
@@ -19,6 +20,8 @@ class blob:
         possible_moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         random.shuffle(possible_moves)
 
+
+        #RESOURCE CONSUMPTION
         # checks for food/resource first so technically gives priority to it i gave it colour green: 0,200,0
         for move in possible_moves:
             new_x = self.x + move[0]
@@ -27,6 +30,7 @@ class blob:
                 if np.array_equal(self.environment.space[new_x, new_y], [0, 200, 0]):
                     self.x, self.y = new_x, new_y
                     self.environment.space[self.x, self.y] = [0, 0, 150]
+                    self.energy += self.food_content
                     self.energy -= self.energy_for_movement
                     return
 
