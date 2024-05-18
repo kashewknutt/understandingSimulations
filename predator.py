4f2d60ffdb5f5456374d3cbdb6689bea476be4a2
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-05-18 23:14:09 +0530

    move function copied from blob in predator

--- predator.py
+++ predator.py
@@ -1,24 +1,42 @@
 
 import random
+import numpy as np
 class predator:
     def __init__(self,gender,age,environmentClassInstance):  #gender is boolean true->male  false->female
         self.gender=gender
         self.age=age
         self.environment=environmentClassInstance
-        self.x = random.randint(0, self.environment.dimension - 1)
-        self.y = random.randint(0, self.environment.dimension - 1)
-        self.environment.space[self.x,self.y] = [255, 0, 0]     #predator is red
+        self.colour=[255, 0, 0]
+        self.environment.space[self.x,self.y] = self.colour     #predator is red
         self.energy=50     #initialised energy to 50 at the start
-    
+        
     
     
     def movement(self):
-        xAdd=random.randint(-1,1)
-        yAdd=random.randint(-1,1)
-        newCoordinate=[self.x+xAdd,self.y+yAdd]
-        if(self.environment.space[newCoordinate[0]][newCoordinate[1]]!=[0,0,0]):
-            self.movement()
-        self.environment.space[newCoordinate[0]][newCoordinate[1]]=[255,0,0]
-        self.environment.space[self.x][self.y]=[0,0,0]
-        self.x=newCoordinate[0]
-        self.y=newCoordinate[1]
+        self.environment.space[self.x, self.y] = [0, 0, 0]
+        
+        possible_moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        random.shuffle(possible_moves)
+
+        # checks for food/resource first so technically gives priority to it i gave it colour green: 0,200,0
+        for move in possible_moves:
+            new_x = self.x + move[0]
+            new_y = self.y + move[1]
+            if 0 <= new_x < self.environment.dimension and 0 <= new_y < self.environment.dimension:
+                if np.array_equal(self.environment.space[new_x, new_y], [0, 0,150]):
+                    self.x, self.y = new_x, new_y
+                    self.environment.space[self.x, self.y] = self.colour
+                    return
+
+        # lets say agar resource doeesnt exist, toh itll automatically move to the available
+        for move in possible_moves:
+            new_x = self.x + move[0]
+            new_y = self.y + move[1]
+            if 0 <= new_x < self.environment.dimension and 0 <= new_y < self.environment.dimension:
+                if np.array_equal(self.environment.space[new_x, new_y], [0, 0, 0]):
+                    self.x, self.y = new_x, new_y
+                    self.environment.space[self.x, self.y] = self.colour
+                    return
+
+        # agar no empty it'll stay wahi; ab kuch nahi ho sakta in deadlock figure out karenge baadme what to do
+        self.environment.space[self.x, self.y] = self.colour
