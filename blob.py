ab6a68356302638a5143be3a24aff3c73524cbbe
Kashew Knutt <kashewknutt@gmail.com>
2024-05-18 21:36:44 +0530

    blob

--- blob.py
+++ blob.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy as np
+
 class blob:
     def __init__(self, gender, age, environmentInstance):  #gender : true = male
         self.gender=gender
@@ -11,13 +13,32 @@ class blob:
 
     def movement(self):
         self.environment.space[self.x, self.y] = [0, 0, 0]
+        
         possible_moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        move = random.choice(possible_moves)
+        random.shuffle(possible_moves)
 
-        new_x = (self.x + move[0]) % self.environment.dimension
-        new_y = (self.y + move[1]) % self.environment.dimension
-        self.x, self.y = new_x, new_y
+        # checks for food/resource first so technically gives priority to it i gave it colour green: 0,200,0
+        for move in possible_moves:
+            new_x = self.x + move[0]
+            new_y = self.y + move[1]
+            if 0 <= new_x < self.environment.dimension and 0 <= new_y < self.environment.dimension:
+                if np.array_equal(self.environment.space[new_x, new_y], [0, 200, 0]):
+                    self.x, self.y = new_x, new_y
+                    self.environment.space[self.x, self.y] = [0, 0, 150]
+                    return
 
+        # lets say agar resource doeesnt exist, toh itll automatically move to the available
+        for move in possible_moves:
+            new_x = self.x + move[0]
+            new_y = self.y + move[1]
+            if 0 <= new_x < self.environment.dimension and 0 <= new_y < self.environment.dimension:
+                if np.array_equal(self.environment.space[new_x, new_y], [0, 0, 0]):
+                    self.x, self.y = new_x, new_y
+                    self.environment.space[self.x, self.y] = [0, 0, 150]
+                    return
+
+        # If no move is possible, stay in the same position
         self.environment.space[self.x, self.y] = [0, 0, 150]
+
 
         
--- environment.py
+++ environment.py
@@ -3,5 +3,4 @@ import numpy as np
 class Environment:
     def __init__(self, dimension):
         self.dimension = dimension
-        # Initialize a 3D numpy array with zeros. Each pixel has 3 values (R, G, B).
         self.space = np.zeros((dimension, dimension, 3), dtype=int)
