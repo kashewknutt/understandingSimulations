d4db4c1bec9579b24054352f6569c6a2d43df109
Kashew Knutt <kashewknutt@gmail.com>
2024-05-19 01:38:47 +0530

    MVP 1.0 realistic

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import matplotlib.animation as animation
 import random
 
 env=Environment(50)
-for _ in range(10):
+for _ in range(50):
     Predator(True, 5, env)
-for _ in range(10):
+for _ in range(60):
     Blob(True, 5, env)
 def update(frame):                           #decrement age after you add the die function for all entities
     #print(env.blobs)
