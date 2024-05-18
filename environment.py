5984699875c4137e735f9b106ceecf235e365d09
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-05-18 20:21:27 +0530

    added stuff

---
+++ environment.py
@@ -0,0 +1,4 @@
+class environment:
+    def __init__(self,dimension):
+        self.dimension=dimension
+        self.space=[dimension][dimension][3]      #matrix of rgb values
---
+++ predator.py
@@ -0,0 +1,5 @@
+from environment import environment
+class predator:
+    def __init(self,gender,age,environment):  #gender is boolean true->male  false->female
+        self.gender=gender
+        self.age=age
