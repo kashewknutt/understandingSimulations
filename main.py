ff667d761519251ebce8591c9fbe6cf0ba39b443
Kashew Knutt <kashewknutt@gmail.com>
2024-05-19 17:04:13 +0530

    MVP 1.3

--- main.py
+++ main.py
@@ -18,26 +18,36 @@ maxFrames = 200
 #Simulation Environment
 env=Environment(50)
 
-
 #selecting random frames for the predators to spawn
 predFrames = [random.randint(0,maxFrames) for _ in range(maxPredators)]
 predFrames.sort()
-
-for _ in range(20):
-    Blob(True, 5, env)
-
+#selecting random frames for the blobs to spawn
+blobFrames = [random.randint(0,maxFrames) for _ in range(maxBlobs)]
+blobFrames.sort()
 
 #increment age after you add the die function for all entities
 def update(frame):
     #print(env.blobs)
 
+    #generating 5 blobs at random frames
+    if frame in blobFrames:
+        for _ in range(5):
+            tempForGender=random.choice([True, False])
+            Blob(tempForGender, 5, env)
+
+    #Blob movement in each frame
     for blob in env.blobs:
         blob.age += 0.2
         blob.movement()
+
+
+    #Predator generating in random frames
     if frame in predFrames:
         tempForGender=random.choice([True, False])
         Predator(tempForGender, 5, env)
     
+
+    #Predator movement in each frame
     for predator in env.predators:
         predator.age += 0.2
         #print(predator)
@@ -52,6 +62,8 @@ def update(frame):
     blob_number_per_frame.append(len(env.blobs))
     predator_number_per_frame.append(len(env.predators))
     return [im]
+
+#plotting the graph
 fig, ax = plt.subplots()
 im = ax.imshow(env.space, animated=True)
 ani = animation.FuncAnimation(fig, update, frames=maxFrames, interval=100, blit=True) #actually animates all the frames
