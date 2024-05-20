f83ff88700cf699ea3139ecc843edb1e70a1c857
Kashew Knutt <kashewknutt@gmail.com>
2024-05-20 16:22:31 +0530

    main1

--- simpy/callCenterCaseStudy/final.py
+++ simpy/callCenterCaseStudy/final.py
@@ -1,12 +1,16 @@
 import main
-
+import matplotlib.pyplot as plt
 
 # Number of times to run the sim
 n = 100
 
 # Customers handled array
-
+customershandled = []
 
 # Run the simulation 10 times
 for i in range(n):
-    main.simulate()
+    customershandled.append(main.simulate())
+
+# Plot the customers handled array using matplotlib
+plt.plot(customershandled, label='Customers')
+plt.show()
