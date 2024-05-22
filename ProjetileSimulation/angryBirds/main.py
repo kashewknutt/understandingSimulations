35ae29831b292b17957236a88c102657b7eff9bf
Kashew Knutt <kashewknutt@gmail.com>
2024-05-22 13:39:03 +0530

    main update

--- ProjetileSimulation/angryBirds/main.py
+++ ProjetileSimulation/angryBirds/main.py
@@ -7,8 +7,10 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 #bird co-ordinates
-bird_x = 111
-bird_y = 326
+birdX = 111
+birdY = 323
+birdRadius = 15
+birdHeld = False
 
 run = True
 while run:
@@ -18,11 +20,19 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-        #event for mouse clicked on bird\
+        #event for mouse clicked on bird
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if bird_x-15 <= pygame.mouse.get_pos()[0] <= bird_x+15 and bird_y-15 <= pygame.mouse.get_pos()[1] <= bird_y+15:
-                bird_x, bird_y = pygame.mouse.get_pos()
-                print("Mouse Clicked", bird_x, bird_y)
+            if birdX - birdRadius <= pygame.mouse.get_pos()[0] <= birdX+birdRadius and birdY-birdRadius <= pygame.mouse.get_pos()[1] <= birdY+birdRadius:
+                birdHeld = True
+                print("Mouse Clicked")
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            birdHeld = False
+            print("Mouse Released")
+    
+    if birdHeld:
+        birdX, birdY = pygame.mouse.get_pos()
+        print("New Cords: ", birdX, birdY)
         
     #background
     screen.fill((255,255,255))
@@ -36,7 +46,7 @@ while run:
 
 
     #bird
-    pygame.draw.circle(screen, (255, 0,0), (bird_x, bird_y), 15)
+    pygame.draw.circle(screen, (255, 0,0), (birdX, birdY), birdRadius)
 
 
     pygame.display.flip()
