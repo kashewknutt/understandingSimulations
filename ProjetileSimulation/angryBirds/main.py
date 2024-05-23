b182baae5d3ed0a37b9d4895405dccec8af5cb0b
Kashew Knutt <kashewknutt@gmail.com>
2024-05-23 14:40:19 +0530

    main

--- ProjetileSimulation/angryBirds/main.py
+++ ProjetileSimulation/angryBirds/main.py
@@ -2,20 +2,24 @@
 import pygame
 import numpy as np
 pygame.init()
-cair=2
+time=pygame.time.Clock()
+cair=2                   
 mass=4
 c=cair/mass
 g=9.8/mass
 k=2
+x=1              #distance pulled
 elastic_constant=np.sqrt(k/mass)
-def velocity_finder(x):
-    return elastic_constant*x
+u=0
+angle=0
+def velocity_finder(xi):
+    return elastic_constant*xi       #how much you pull it back->x
 def xcord(u,s,t):                                   #u and s are the speeds and the angles
-    return originalcords[0]+(u * np.cos(s) / c) * (1 - np.exp(-c * t))
+    return originalcords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * t))
 def ycord(u,s,t):
-    term1 = u * np.sin(s) + g / c
-    term2 = (u * np.sin(s) + g / c) * np.exp(-c * t)
-    return originalcords[1]-(term1 - term2) / c - g * t / c
+    term1 = u * (np.sin(s)) + g / c
+    term2 = (u * (np.sin(s)) + g / c) * np.exp(-c * t)
+    return originalcords[1]-((term1 - term2) / c) - g * t / c
 screen = pygame.display.set_mode((800,600))
 
 #bird co-ordinates
@@ -23,7 +27,7 @@ originalcords=np.array([111,323])
 currentcords=np.array([111,323])
 birdRadius = 15
 birdHeld = False
-
+BirdFlying=False
 run = True
 while run:
     for event in pygame.event.get():
@@ -41,13 +45,21 @@ while run:
         if event.type == pygame.MOUSEBUTTONUP:
             birdHeld = False
             print("Mouse Released")
-    
+            BirdFlying=True
+            startTime=pygame.time.get_ticks() / 1000
+            u=velocity_finder(x)
+            print("u", u,"angle", angle)
+
     if birdHeld:
         currentcords[0], currentcords[1] = pygame.mouse.get_pos()
         x=np.linalg.norm(currentcords-originalcords)         #displays x
-        theta=np.arctan(currentcords[1]/currentcords[0])
+        angle = np.arctan2(originalcords[1] - currentcords[1], originalcords[0] - currentcords[0])       #edit it so that it outputs negative 
         print("New Cords distance: ",x)
-        
+    if BirdFlying:
+        t=(pygame.time.get_ticks() / 1000)-startTime
+        currentcords[0]=xcord(u,angle,t)
+        currentcords[1]=ycord(u,angle,t)
+
     #background
     screen.fill((255,255,255))
 
