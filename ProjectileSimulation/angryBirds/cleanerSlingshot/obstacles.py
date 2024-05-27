3b78c7c7b1da99e626b88173504cf9def64104ac
Kashew Knutt <kashewknutt@gmail.com>
2024-05-27 13:06:17 +0530

    spelling mistake in parent folder

--- ProjectileSimulation/angryBirds/cleanerSlingshot/obstacles.py
+++ ProjectileSimulation/angryBirds/cleanerSlingshot/obstacles.py
@@ -5,8 +5,16 @@ class Obstacle:
         self.screen = screen
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
         self.durability = durability
-        
+
+    #code of the obstacle to naturally fall to the ground (call it directly in main.py)
+    def falling(self):
+        if self.y + self.height <= 400:
+            self.y += 1
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
 
